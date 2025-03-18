12cf991e51c8668c2e231553c5d36e72664e3694
eozkan2004 <mrhnzkn@gmail.com>
2025-03-18 22:44:10 +0300

    Create RatingAndComment.java

---
+++ backend_files/src/main/java/org/project/model/product_model/RatingAndComment.java
@@ -0,0 +1,18 @@
+package org.project.model.product_model;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+
+public class RatingAndComment {
+
+    private int rating;
+    private String comment;
+    
+}

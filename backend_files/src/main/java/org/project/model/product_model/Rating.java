c2b7714090ef6c24830bab53c02752351bb074d0
Emirhan Özkan <mrhnzkn@gmail.com>
2025-03-25 11:26:58 +0300

    Update Rating.java

--- backend_files/src/main/java/org/project/model/product_model/Rating.java
+++ backend_files/src/main/java/org/project/model/product_model/Rating.java
@@ -1,1 +1,28 @@
+package org.project.model.product_model;
 
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Document(collection = "ratings")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+
+public class Rating {
+
+    @Id
+    private String rating_id;
+    private String productId;
+    private String userId;
+    private int rating;
+    private LocalDateTime date;
+    
+}

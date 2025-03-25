b850c8cc425c603cd471dfb5edb770df9d265123
Emirhan Özkan <mrhnzkn@gmail.com>
2025-03-25 11:22:02 +0300

    Update RatingRepository.java

--- backend_files/src/main/java/org/project/repository/RatingRepository.java
+++ backend_files/src/main/java/org/project/repository/RatingRepository.java
@@ -1,1 +1,12 @@
+package org.project.repository;
 
+import org.project.model.product_model.Rating;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+
+public interface RatingRepository extends MongoRepository<Rating, String>{
+    List<Rating> findByUserIdAndProductId(String user_id, String product_id);
+    List<Rating> findByProductId(String product_id);
+}

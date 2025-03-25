fb7f07d5b614308d2f05bfff79c274929f2817ba
Emirhan Özkan <mrhnzkn@gmail.com>
2025-03-25 11:26:08 +0300

    Update Comment.java

--- backend_files/src/main/java/org/project/model/product_model/Comment.java
+++ backend_files/src/main/java/org/project/model/product_model/Comment.java
@@ -1,1 +1,28 @@
+package org.project.model.product_model;
 
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Document(collection = "comments")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+
+public class Comment {
+
+    @Id
+    private String comment_id;
+    private String productId;
+    private String userId;
+    private String content;
+    private LocalDateTime date;
+    
+}

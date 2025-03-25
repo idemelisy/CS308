9884a06acd8eeac18051f49d708e46858c1094fe
Emirhan Özkan <mrhnzkn@gmail.com>
2025-03-25 14:23:20 +0300

    Update ProductService.java

--- backend_files/src/main/java/org/project/service/ProductService.java
+++ backend_files/src/main/java/org/project/service/ProductService.java
@@ -8,38 +8,127 @@ import org.springframework.stereotype.Service;
 
 import java.util.List; 
 
-            // genel olarak input almaktansa ekranda görünen ürünler üzerinden gideriz diye 
-            // exception kontrolü yapmadım
+
 @Service
 public class ProductService {
 
     @Autowired
     private ProductRepository product_repo;
+    @Autowired
+    private CommentRepository comment_repo;
+    @Autowired
+    private RatingRepository rating_repo;
+
+    private String generate_id() {
+        return UUID.randomUUID().toString();
+    }
 
     public Product find_product(String product_id){
-        return product_repo.findById(product_id).get();
+        return product_repo.findById(product_id).get();   
     }
 
     public List<Product> list_all_products(){
         return product_repo.findAll();
     }
 
-    public List<RatingAndComment> past_user_experience(String product_id){
-        return product_repo.findById(product_id).get().getRatings_and_comments();
+
+
+
+    public String add_comment_and_rating(String user_id, String product_id, String comment, int rating){
+        String mutual_id = generate_id();
+        LocalDateTime now = LocalDateTime.now();
+
+        Rating new_rating = new Rating(mutual_id, product_id, user_id, rating, now);
+        Comment new_comment = new Comment(mutual_id, product_id, user_id, comment, now);
+
+        rating_repo.save(new_rating);
+        comment_repo.save(new_comment);
+
+        return mutual_id + " " + product_id + " " + user_id;
+    }
+
+    public Comment add_only_comment(String user_id, String product_id, String content){
+        String comment_id = generate_id();
+        LocalDateTime now = LocalDateTime.now();
+
+        return comment_repo.save(new Comment(comment_id, product_id, user_id, content, now));
+    }
+
+    public Rating add_only_rating(String product_id, String user_id, int rating){
+        String rating_id = generate_id();
+        LocalDateTime now = LocalDateTime.now();
+
+        return rating_repo.save(new Rating(rating_id, product_id, user_id, rating, now));
+    }
+
+
+
+
+    public List<ReviewObject> get_merged_reviews(String product_id){
+        List<Rating> ratings = list_product_ratings(product_id);
+        List<Comment> comments = list_product_comments(product_id);
+
+        List<ReviewObject> merged_reviews = new ArrayList<>();
+
+        for(Rating rating: ratings){
+            ReviewObject new_review = new ReviewObject();
+            new_review.setRating_id(rating.getRating_id());
+            new_review.setProductId(rating.getProductId());
+            new_review.setUserId(rating.getUserId());
+            new_review.setRating(rating.getRating());
+            new_review.setDate(rating.getDate());
+            merged_reviews.add(new_review);
+        }
+
+        for (Comment comment: comments){
+            boolean matched = false;
+            
+            for(ReviewObject review: merged_reviews){
+                if(review.getUserId().equals(comment.getUserId()) && 
+                review.getProductId().equals(comment.getProductId()) &&
+                review.getDate().equals(comment.getDate())){
+
+                    review.setComment_id(comment.getComment_id());
+                    review.setContent(comment.getContent());
+                    matched = true;
+                    break;
+                }
+            }
+
+            if(!matched){
+                ReviewObject newReview = new ReviewObject();
+                newReview.setComment_id(comment.getComment_id());
+                newReview.setProductId(comment.getProductId());
+                newReview.setUserId(comment.getUserId());
+                newReview.setDate(comment.getDate());
+                
+                merged_reviews.add(newReview);
+            }
+        }
+
+        return merged_reviews;
+    }
+
+    public List<Comment> list_product_comments(String product_id){
+        return comment_repo.findByProductId(product_id);
+    }
+
+    public List<Rating> list_product_ratings(String product_id){
+        return rating_repo.findByProductId(product_id);
     }
 
     public double calculate_average_rating(String product_id){
 
-        List<RatingAndComment> ratings_and_comments = past_user_experience(product_id);
+        List<Rating> ratings = list_product_ratings(product_id);
         int total = 0;
-        if (ratings_and_comments == null || ratings_and_comments.isEmpty()) {
+        if (ratings == null || ratings.isEmpty()) {
             return 0.0;
         }
-        for (RatingAndComment iterator: ratings_and_comments){
-            total += iterator.getRating();     // comment bırakılırsa rating zorunludur mantığıyla null kontrolü yapmadım
+        for (Rating iterator: ratings){
+            total += iterator.getRating();     
         }
 
-        return (double) total / ratings_and_comments.size();
+        return (double) total / ratings.size();
     }
 /*
     public Product addProduct(Product newProduct) {

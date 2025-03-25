436c9edc2d69da2693a2b14c03cda19249cdf10f
Emirhan Özkan <mrhnzkn@gmail.com>
2025-03-25 11:21:37 +0300

    Update CommentRepository.java

--- backend_files/src/main/java/org/project/repository/CommentRepository.java
+++ backend_files/src/main/java/org/project/repository/CommentRepository.java
@@ -1,1 +1,11 @@
+package org.project.repository;
 
+import org.project.model.product_model.Comment;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface CommentRepository extends MongoRepository<Comment, String>{
+    List<Comment> findByUserIdAndProductId(String user_id, String product_id);
+    List<Comment> findByProductId(String product_id);
+}

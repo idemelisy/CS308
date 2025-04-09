d2662ef3aee5929f3d52b4cae92988eb5d659d9f
Emirhan Özkan <mrhnzkn@gmail.com>
2025-04-09 16:32:33 +0300

    Create ShoppingHistory.java

---
+++ backend_files/src/main/java/org/project/repository/ShoppingHistory.java
@@ -0,0 +1,8 @@
+package org.project.repository;
+
+import org.project.model.Invoice;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface ShoppingHistory extends MongoRepository<Invoice, String>{
+    
+}

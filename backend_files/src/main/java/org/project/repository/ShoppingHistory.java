6d549e79c8aae794a2ffcc9cb4341358f14bbf5e
Emirhan Özkan <mrhnzkn@gmail.com>
2025-04-09 16:38:44 +0300

    Update ShoppingHistory.java

--- backend_files/src/main/java/org/project/repository/ShoppingHistory.java
+++ backend_files/src/main/java/org/project/repository/ShoppingHistory.java
@@ -1,8 +1,11 @@
 package org.project.repository;
 
+import java.util.List;
+
+import org.project.model.Customer;
 import org.project.model.Invoice;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface ShoppingHistory extends MongoRepository<Invoice, String>{
-    
+        public List<Invoice> findByPurchaser(Customer purchaser);
 }

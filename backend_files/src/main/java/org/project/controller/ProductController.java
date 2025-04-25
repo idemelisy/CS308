78006283cfcf43a37b4bddbced9c1903e901843a
Emirhan Özkan <mrhnzkn@gmail.com>
2025-04-25 16:55:21 +0300

    Update ProductController.java

--- backend_files/src/main/java/org/project/controller/ProductController.java
+++ backend_files/src/main/java/org/project/controller/ProductController.java
@@ -1,8 +1,6 @@
 package org.project.controller;
 
-import org.project.model.product_model.Comment;
 import org.project.model.product_model.Product;
-import org.project.model.product_model.Rating;
 import org.project.model.product_model.ReviewObject;
 import org.project.service.ProductService;
 
@@ -55,27 +53,17 @@ public class ProductController {
 
 
 
-    @GetMapping("/{product_id}/reviews")
-    public List<ReviewObject> getProductReviews(@PathVariable("product_id") String product_id) {
+    @GetMapping("/reviews")
+    public List<ReviewObject> getProductReviews(@RequestParam String product_id) {
         return productService.get_merged_reviews(product_id);
     }
 
     @GetMapping("/{product_id}/average_rating")
-    public double getAverageRating(@PathVariable("product_id") String product_id) {
+    public double getAverageRating(@RequestParam String product_id) {
         return productService.calculate_average_rating(product_id);
     }
 
-    @PostMapping("/add_only_comment")
-    public Comment add_comment(@RequestBody Comment only_comment){
-        return productService.add_only_comment(only_comment.getProductId(), only_comment.getUserId(), only_comment.getContent());   
-    }
-
-    @PostMapping("/add_only_rating")
-    public Rating add_rating(@RequestBody Rating only_rating){
-        return productService.add_only_rating(only_rating.getProductId(), only_rating.getUserId(), only_rating.getRating());
-    } 
-
-    @PostMapping("/add_both")
+    @PostMapping("/add-comment-rating")
     public String add_comment_rating(@RequestBody ReviewObject review_object){
         return productService.add_comment_and_rating(
             review_object.getUserId(), 
@@ -92,14 +80,10 @@ public class ProductController {
         return productService.addProduct(newProduct);
     }
 
-    @PutMapping("/{product_id}/update")
-    public Product updateProduct(@PathVariable("product_id") String product_id, @RequestBody Product updatedProduct) {
-        return productService.updateProduct(product_id, updatedProduct);
-    }
-
     @DeleteMapping("/{product_id}/delete")
     public String deleteProduct(@PathVariable("product_id") String product_id) {
         return productService.deleteProduct(product_id);
     }
 
 }
+

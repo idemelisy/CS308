d52688bfb834536b3f0735497cedd649b44a225b
Emirhan Özkan <mrhnzkn@gmail.com>
2025-05-05 20:27:12 +0300

    Update Customer.java

--- backend_files/src/main/java/org/project/model/Customer.java
+++ backend_files/src/main/java/org/project/model/Customer.java
@@ -19,10 +19,12 @@ public class Customer extends User{
 
     @Field("customer_cart")
     private HashMap<String, Integer> shopping_cart;
+    private HashSet<String> wishlist;
 
     public Customer(String account_id, String name, String surname, String email, String password){
         super(account_id, name, surname, email, password);
-        this.shopping_cart = null;
+        this.shopping_cart = new HashMap<String, Integer>();
+        this.wishlist = new HashSet<String>();
     }
 
 }

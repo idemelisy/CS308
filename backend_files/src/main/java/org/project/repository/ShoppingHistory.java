6248130e16b1ec35b551bf95b471cebac2de242a
Emirhan Özkan <mrhnzkn@gmail.com>
2025-05-20 20:27:20 +0300

    Update ShoppingHistory.java

--- backend_files/src/main/java/org/project/repository/ShoppingHistory.java
+++ backend_files/src/main/java/org/project/repository/ShoppingHistory.java
@@ -1,9 +1,14 @@
 package org.project.repository;
 
 
+import java.time.Instant;
+import java.util.List;
+
 import org.project.model.Invoice;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+
 public interface ShoppingHistory extends MongoRepository<Invoice, String>{
     Invoice findByInvoiceId(String InvoiceId);
+    List<Invoice> findByDateBetween(Instant startDate, Instant endDate);
 }
